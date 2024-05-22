0d5df2a48847fd74ea41dc0e06095f3719e2ceb6
Ekaterina_Varlamova <katy1781@inbox.ru>
2024-05-22 03:34:54 +0300

    fix statistics

--- src/GatewayService/NetUI/producer/KafkaProducer.cpp
+++ src/GatewayService/NetUI/producer/KafkaProducer.cpp
@@ -1,4 +1,8 @@
 #include "KafkaProducer.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 void KafkaProducer::PostToBroker(const LogString& logData) {
     json_t j;
@@ -11,11 +15,21 @@ void KafkaProducer::PostToBroker(const LogString& logData) {
     j["duration"] = logData.duration;
 
     std::string payload = j.dump();
-
-
     std::string escaped_string = oatpp::encoding::Base64::encode(payload.c_str(), strlen(payload.c_str()));
 
     LoggerFactory::GetLogger()->LogInfo((std::string("python3 /app/NetUI/producer/producer.py ") + brokers + " " + topic + " " + escaped_string).c_str());
-    system((std::string("python3 /app/NetUI/producer/producer.py ") + brokers + " " + topic + " " + escaped_string).c_str());
+    
+    pid_t pid = fork();
+    
+    if (pid == 0) {
+        system((std::string("python3 /app/NetUI/producer/producer.py ") + brokers + " " + topic + " " + escaped_string).c_str());
+        exit(0);
+    } else if (pid > 0) {
+        int status;
+        sleep(1);
+        if (waitpid(pid, &status, WNOHANG) == 0) {
+            kill(pid, SIGKILL);
+        }
+    }
 
 }
